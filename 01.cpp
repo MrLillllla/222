88a3a4deadc00b8cdadc28e28c799ce0fde640cd
Mr.li <1207094452@qq.com>
2023-05-09 19:22:49 +0800

    增加test2函数，打印传入的n的对应值

--- 01.cpp
+++ 01.cpp
@@ -4,8 +4,16 @@ void test()
 {
  cout<<"janlile"<<endl;
 }
+void test2(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<i+100<<endl;
+    }
+}
 int main()
 {
     test();
+    test2(4);
     return 0;
 }
